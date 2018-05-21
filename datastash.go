743075ea7e89a95db2a5a61cf46d9e69f7da54b1
Hooks Zhang <hookszhang@gmail.com>
2018-05-21 16:47:52 +0800

    Update glide depends

--- datastash.go
+++ datastash.go
@@ -1,8 +1,12 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/hudl/fargo"
+)
 
 func main() {
+	enableEurekaClient()
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
@@ -10,4 +14,11 @@ func main() {
 		})
 	})
 	r.Run(":9999") // listen and serve on 0.0.0.0:9999
+}
+
+func enableEurekaClient() {
+	c := fargo.NewConn("http://localhost:8761/eureka/v2")
+	c.RegisterInstance(&fargo.Instance{
+		App: "datastash",
+	})
 }
